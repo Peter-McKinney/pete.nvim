7ad51082adaa32b30b9147a6ff17648602193608
Pete McKinney <91989516+petemckinneysb@users.noreply.github.com>
2023-11-10 14:07:24 -0600

    Add telescope extension for rip grep args.

--- init.lua
+++ init.lua
@@ -331,11 +331,14 @@ vim.keymap.set('n', '<leader>pa', '[[<Cmd>let @+=expand(\'%:p\')<CR>]]', { desc 
 vim.keymap.set('n', '<leader>pr', '[[<Cmd>let @+=expand(\'%:t\')<CR>]]', { desc = 'Copy file name', silent = true })
 
 -- [[ sessions ]]
+-- telescope extension??
 vim.keymap.set('n', '<leader>ss', ':mksession! ~/vim-sessions/pete-session.vim<CR>', {desc = '[S]ave [S]ession Quick'})
 vim.keymap.set('n', '<leader>sr', ':source ~/vim-sessions/pete-session.vim<CR>', {desc = '[R]estore [S]ession Quick'})
 
 vim.keymap.set('n', '<leader>sns', ':mksession! ~/vim-sessions/', {desc = '[S]ave [S]ession name'})
 vim.keymap.set('n', '<leader>snr', ':source ~/vim-sessions/', {desc = '[R]estore [S]ession name'})
+
+vim.g.loaded_netrwPlugin = 1
 
 -- [[ Configure autopairs ]]
 require('nvim-autopairs').setup()
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,8 +5,22 @@
 return {
 	{ "windwp/nvim-autopairs" },
 	{
-	    "iamcco/markdown-preview.nvim",
-	    run = function() vim.fn["mkdp#util#install"]() end,
+		"iamcco/markdown-preview.nvim",
+		run = function() vim.fn["mkdp#util#install"]() end,
 	},
-	{ "tpope/vim-surround"}
+	{ "tpope/vim-surround" },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+		},
+		config = function()
+			require("telescope").load_extension("live_grep_args")
+		end
+	}
 }
