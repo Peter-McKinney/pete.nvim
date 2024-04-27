f31d58e4e9f261d3a06e1e28937e4d7105e6432d
Pete McKinney <91989516+petemckinneysb@users.noreply.github.com>
2024-04-27 10:28:12 -0500

    Treesitter, angularls, prettier formatting (#8)

---
+++ lua/custom/telescope_pickers.lua
@@ -0,0 +1,79 @@
+local pickers = require 'telescope.pickers'
+local finders = require 'telescope.finders'
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
+local previewers = require 'telescope.previewers'
+
+local M = {}
+
+local function get_git_diff_data(command)
+  local output = vim.fn.system(command)
+  local files = {}
+  local current_file
+  for line in output:gmatch '[^\r\n]+' do
+    if line:match '^diff' then
+      current_file = line:match 'b/(.*)'
+      files[current_file] = {}
+    elseif current_file then
+      table.insert(files[current_file], line)
+    end
+  end
+  return files
+end
+
+local function create_diff_picker(diff_data)
+  local results = {}
+  for file, hunks in pairs(diff_data) do
+    table.insert(results, {
+      value = file,
+      ordinal = file,
+      display = file,
+      diff_hunks = hunks,
+    })
+  end
+
+  pickers
+    .new({}, {
+      prompt_title = 'Git Diff Files',
+
+      finder = finders.new_table {
+        results = results,
+        entry_maker = function(entry)
+          return entry
+        end,
+      },
+      sorter = require('telescope.sorters').get_fuzzy_file(),
+      previewer = previewers.new_buffer_previewer {
+        define_preview = function(self, entry, status)
+          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, true, entry.diff_hunks)
+          vim.api.nvim_buf_set_option(self.state.bufnr, 'filetype', 'diff')
+        end,
+      },
+      attach_mappings = function(prompt_bufnr, map)
+        map('i', '<CR>', function()
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          vim.cmd('edit ' .. selection.value)
+        end)
+        map('n', '<CR>', function()
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          vim.cmd('edit ' .. selection.value)
+        end)
+        return true
+      end,
+    })
+    :find()
+end
+
+M.git_diff_files = function()
+  local diff_data = get_git_diff_data 'git diff HEAD^ HEAD'
+  create_diff_picker(diff_data)
+end
+
+M.git_diff_develop = function()
+  local diff_data = get_git_diff_data 'git diff develop'
+  create_diff_picker(diff_data)
+end
+
+return M
