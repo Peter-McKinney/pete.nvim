6d1da1b01ef80de8ba42506c561dfb42a2266d54
Pete McKinney <91989516+petemckinneysb@users.noreply.github.com>
2025-04-25 18:13:30 -0500

    Lots of updates from main fork

--- lua/custom/open_coverage_report.lua
+++ lua/custom/open_coverage_report.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local function escape_pattern(text)
-  return text:gsub("([%%%^%$%(%)%.%[%]%*%+%-%?])", "%%%1")
+  return text:gsub('([%%%^%$%(%)%.%[%]%*%+%-%?])', '%%%1')
 end
 
 local function get_current_line()
@@ -10,25 +10,25 @@ local function get_current_line()
 end
 
 local function get_file_path()
-  local prefix_to_remove = "/Users/43159/github/qpp-submission-client"
+  local prefix_to_remove = '/Users/43159/github/qpp-submission-client'
 
   local file_path = vim.api.nvim_buf_get_name(0)
-  local sanitized_file_path = string.gsub(file_path, escape_pattern(prefix_to_remove), "")
+  local sanitized_file_path = string.gsub(file_path, escape_pattern(prefix_to_remove), '')
 
   return sanitized_file_path
 end
 
 local function get_line_fragment_identifier()
   local line_number = get_current_line()
-  return string.format("#L%s", line_number)
+  return string.format('#L%s', line_number)
 end
 
 M.open_coverage_at_current_line = function()
   local line_number_identifier = get_line_fragment_identifier()
   local file_path = get_file_path()
-  local coverage_path = "http://localhost:8080" .. file_path .. ".html" .. line_number_identifier
+  local coverage_path = 'http://localhost:8080' .. file_path .. '.html' .. line_number_identifier
   print(coverage_path)
-  vim.fn.system("open " .. coverage_path)
+  vim.fn.system('open ' .. coverage_path)
 end
 
 return M
--- lua/custom/plugins/autocomplete.lua
+++ lua/custom/plugins/autocomplete.lua
@@ -110,6 +110,7 @@ return {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'nvim_lsp_signature_help' },
         },
       }
     end,
